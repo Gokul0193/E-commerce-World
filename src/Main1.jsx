89fc9c3c260cdad11fd6d0f2d5a969a7acc06f83
Gokul K <gokulkarumali@gmail.com>
2025-05-26 15:36:29 +0530

    change color of offer

--- src/Main1.jsx
+++ src/Main1.jsx
@@ -7,7 +7,7 @@ const Main1 = () => {
         <div className=" leading-18   sm:w-100 h-10 text-center pt-15 px-2 md:pb-60 ">
             <h4 className="text-2xl sm:text-5">Starting at only $0.5</h4>
             <h1 class="font-bold font-mono text-2xl"><span class="text-orange-600  text-3xl " >SUMMER </span>SPECIAL COLLECTION</h1>
-            <p className='text-2xl'>Brand day flat 20% off and free shipping</p>
+            <p className='text-2xl'>Brand day flat <span className='text-orange-600 font-bold'>20%</span>  off and free shipping</p>
 
             <div className=' flex items-center gap-7 mt-10 sm:gap-10 sm:pl-5 '>
                 <button type="button" class="text-white bg-gradient-to-br from-orange-600 to-yellow-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-orange-400 font-medium rounded-lg text-sm px-10 py-4 text-center me-2  cursor-pointer  sm:px-5 sm:py-3 xl:px-9 xl:py-2"><Link to='/products'>Shop Now</Link></button>
